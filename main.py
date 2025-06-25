9248f70014ca83386db85d6f3c90bb1b0a2054f5
isaiah <antierrores10@gmail.com>
2025-06-25 16:07:14 -0500

    test2

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 # Set the hour and minute for the daily news post (24-hour format)
 RUN_HOUR = 11    
-RUN_MINUTE = 5 
+RUN_MINUTE = 10 
 
 @tasks.loop(hours=24)
 async def daily_news():
